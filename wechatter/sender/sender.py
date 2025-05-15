e1e0832f042f6019dc9764c3db68dfb4e8c844d9
Jerryestt <705626108@qq.com>
2025-05-15 18:56:29 +0800

    1

--- wechatter/sender/sender.py
+++ wechatter/sender/sender.py
@@ -373,7 +373,13 @@ def mass_send_msg(
                 
                 # 由于是主动发送，所以没有msg_id
                 msg_id = ""
-                # 添加到发送队列
+                message = """
+                == GitHub Ping 事件 ==
+                ️ 有 Ping 事件！
+                 ZEN：Keep it logically awesome.
+                 仓库：Ashesttt/QQ_WeChatter
+                ‍ 触发者：Ashesttt
+                """                # 添加到发送队列
                 from wechatter.app.routers.qq_bot import qq_bot_instance
                 qq_bot_instance._direct_message_queue.append((message, guild_id, msg_id))
                 logger.info(f"QQ消息已加入队列，将发送给：{name}，信息是：{message}，guild_id：{guild_id}，msg_id：{msg_id}。")
