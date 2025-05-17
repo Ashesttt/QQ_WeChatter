5a42208efacaa036ac9f049199738cfc6fe0e491
Jerryestt <705626108@qq.com>
2025-05-17 21:47:51 +0800

    feat(sender): 优化 URL 消息处理逻辑
    - 修改了引用消息和 URL 消息的处理流程
    - 增加了对纯 URL 消息的处理，将其转换为二维码
    -优化了代码结构，提高了可读性和可维护性

--- wechatter/sender/sender.py
+++ wechatter/sender/sender.py
@@ -189,15 +189,28 @@ def _send_msg1(
     """    
     # 一般只要是引用的消息，message都是url，但是qq机器人需要配置https才可以发送url，
     # 因此如果是引用消息，就可以把它变成二维码，这样就很好的解决问题
+    # if quoted_response:
+    #     message = make_quotable(message=message, quoted_response=quoted_response)
+    #     message = get_qrcode_saved_path(message)
+    #     # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
+    #     # 注意type要用"localfile"
+    #     type="localfile"
+    # else:    
+    #     # 不是引用消息，就进行url脱敏处理
+    #     # 对消息进行url脱敏处理
+    #     message = desensitize_message(message)
+        
     if quoted_response:
-        message = make_quotable(message=message, quoted_response=quoted_response)
-        message = get_qrcode_saved_path(message)
-        # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
-        # 注意type要用"localfile"
-        type="localfile"
-    else:    
-        # 不是引用消息，就进行url脱敏处理
-        # 对消息进行url脱敏处理
+        # 如果内容是URL，转二维码
+        if message.startswith("http://") or message.startswith("https://"):
+            qrcode_path = get_qrcode_saved_path(message)
+            # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
+            # 注意type要用"localfile"
+            type="localfile"
+            return
+        else:
+            message = make_quotable(message=message, quoted_response=quoted_response)
+    else:
         message = desensitize_message(message)
         
     is_image = False
@@ -369,15 +382,27 @@ def mass_send_msg(
 
     # 一般只要是引用的消息，message都是url，但是qq机器人需要配置https才可以发送url，
     # 因此如果是引用消息，就可以把它变成二维码，这样就很好的解决问题
+    # if quoted_response:
+    #     message = make_quotable(message=message, quoted_response=quoted_response)
+    #     message = get_qrcode_saved_path(message)
+    #     # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
+    #     # 注意type要用"localfile"
+    #     type="localfile"
+    # else:
+    #     # 不是引用消息，就进行url脱敏处理
+    #     # 对消息进行url脱敏处理
+    #     message = desensitize_message(message)
     if quoted_response:
-        message = make_quotable(message=message, quoted_response=quoted_response)
-        message = get_qrcode_saved_path(message)
-        # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
-        # 注意type要用"localfile"
-        type="localfile"
+        # 如果内容是URL，转二维码
+        if message.startswith("http://") or message.startswith("https://"):
+            qrcode_path = get_qrcode_saved_path(message)
+            # 成功把url变成qrcode 注意这里的message已经变成了qrcode的路径
+            # 注意type要用"localfile"
+            type="localfile"
+            return
+        else:
+            message = make_quotable(message=message, quoted_response=quoted_response)
     else:
-        # 不是引用消息，就进行url脱敏处理
-        # 对消息进行url脱敏处理
         message = desensitize_message(message)
         
     # 由于是主动发送，所以没有msg_id
